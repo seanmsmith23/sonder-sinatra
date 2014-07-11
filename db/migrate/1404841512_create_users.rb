ef29c9a117cf8244a38ce917a05fb0bf9d20c9ca
Sean Smith <seansmith1020@gmail.com>
2014-07-11 13:19:24 -0600

    changed table/users/username to email and added unique constraint

--- db/migrate/1404841512_create_users.rb
+++ db/migrate/1404841512_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def up
     create_table :users do |t|
-      t.string :username
+      t.string :email
       t.string :password
     end
+    add_index :users, :email, unique: true
   end
 
   def down
