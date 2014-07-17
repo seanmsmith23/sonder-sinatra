c8f43f141db3ceef2f49fe3388da6471890b857b
Sean Smith <seansmith1020@gmail.com>
2014-07-17 09:47:52 -0600

    added first and last to users

--- db/migrate/1404841512_create_users.rb
+++ db/migrate/1404841512_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def up
     create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
       t.string :email
       t.string :password
     end
