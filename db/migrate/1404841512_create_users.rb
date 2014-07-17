6bcb5375d58330ddf3ccd24a48e13cccad39f144
Sean Smith <seansmith1020@gmail.com>
2014-07-17 08:41:25 -0600

    added memories table

--- db/migrate/1404841512_create_users.rb
+++ db/migrate/1404841512_create_users.rb
@@ -17,11 +17,18 @@ class CreateUsers < ActiveRecord::Migration
       t.integer :user_id
       t.integer :memorial_id
     end
+
+    create_table :memories do |t|
+      t.integer :user_id
+      t.integer :memorial_id
+      t.string :memory
+    end
   end
 
   def down
     drop_table :users
     drop_table :memorials
     drop_table :users_memorials
+    drop_table :memories
   end
 end
